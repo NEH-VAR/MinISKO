96be0f6a14bed072857c43cc184e80b49a0c592a
NEH-VAR <ravhen.yt@gmail.com>
2024-11-25 02:55:59 +0800

    COMMIT

--- script/transport.js
+++ script/transport.js
@@ -1,84 +1,94 @@
 function showMatrix(type) {
   const matrixDiv = document.getElementById("matrix");
-  if (type === "jeepney") {
-    matrixDiv.innerHTML = `<h3>Jeepney Fare Matrix</h3>
-            <div id="fare-wrapper">
-                <div id="fare-header">
-                    <h4 class="route">Route</h4>
-                    <h4 class="fare">Fare</h4>
-                </div>
-                <div id="bar"></div>
-                <div class="fare-content">
-                    <div class="fare-row">
-                        <p class="route">Holy Spirit Hospital</p>
-                        <p class="fare">₱10.00</p>
-                    </div>
-                    <div class="fare-row">
-                        <p class="route">Holy Spirit Hospital to Admin</p>
-                        <p class="fare">₱15.00</p>
-                    </div>
-                </div>
-            </div>`;
-  } else if (type === "tricycle") {
-    matrixDiv.innerHTML = `<h3>Tricycle Fare Matrix</h3>
-            <div id="fare-wrapper">
-                <div id="fare-header">
-                    <h4 class="route">Route</h4>
-                    <h4 class="fare">Fare</h4>
-                </div>
-                <div id="bar"></div>
-                <div class="fare-content">
-                    <div class="fare-row" id="tricycle-1" onclick="tricycleMap(1)">
-                        <p class="route">Holy Spirit to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
-                        <p class="fare">₱25.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-2" onclick="tricycleMap(2)">
-                        <p class="route">Holy Spirit to CSM</p>
-                        <p class="fare">₱30.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-3" onclick="tricycleMap(3)">
-                        <p class="route">Holy Spirit to Sports Complex</p>
-                        <p class="fare">₱40.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-4" onclick="tricycleMap(4)">
-                        <p class="route">One Big Wash to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
-                        <p class="fare">₱15.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-5" onclick="tricycleMap(5)">
-                        <p class="route">One Big Wash to CSM</p>
-                        <p class="fare">₱20.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-6" onclick="tricycleMap(6)">
-                        <p class="route">One Big Wash to Sports Complex</p>
-                        <p class="fare">₱20.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-7" onclick="tricycleMap(7)">
-                        <p class="route">Admin to CSM</p>
-                        <p class="fare">₱15.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-8" onclick="tricycleMap(8)">
-                        <p class="route">Admin to Sports Complex</p>
-                        <p class="fare">₱20.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-9" onclick="tricycleMap(9)">
-                        <p class="route">CSM to Sports Complex</p>
-                        <p class="fare">₱15.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-10" onclick="tricycleMap(10)">
-                        <p class="route">Bago Oshiro to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
-                        <p class="fare">₱25.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-11" onclick="tricycleMap(11)">
-                        <p class="route">Bago Oshiro to CSM</p>
-                        <p class="fare">₱30.00</p>
-                    </div>
-                    <div class="fare-row" id="tricycle-12" onclick="tricycleMap(12)">
-                        <p class="route">Bago Oshiro to Sports Complex</p>
-                        <p class="fare">₱35.00</p>
-                    </div>
-                </div>
-            </div>`;
-  }
+
+  // Remove the visible class to start the fade-out effect
+  matrixDiv.classList.remove("visible");
+
+  // Use a timeout to allow the fade-out effect to complete before changing the content
+  setTimeout(() => {
+      if (type === "jeepney") {
+          matrixDiv.innerHTML = `<h3>Jeepney Fare Matrix</h3>
+              <div id="fare-wrapper">
+                  <div id="fare-header">
+                      <h4 class="route">Route</h4>
+                      <h4 class="fare">Fare</h4>
+                  </div>
+                  <div id="bar"></div>
+                  <div class="fare-content">
+                      <div class="fare-row">
+                          <p class="route">Holy Spirit Hospital</p>
+                          <p class="fare">₱10.00</p>
+                      </div>
+                      <div class="fare-row">
+                          <p class="route">Holy Spirit Hospital to Admin</p>
+                          <p class="fare">₱15.00</p>
+                      </div>
+                  </div>
+              </div>`;
+      } else if (type === "tricycle") {
+          matrixDiv.innerHTML = `<h3>Tricycle Fare Matrix</h3>
+              <div id="fare-wrapper">
+                  <div id="fare-header">
+                      <h4 class="route">Route</h4>
+                      <h4 class="fare">Fare</h4>
+                  </div>
+                  <div id="bar"></div>
+                  <div class="fare-content">
+                      <div class="fare-row" id="tricycle-1" onclick="tricycleMap(1)">
+                          <p class="route">Holy Spirit to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
+                          <p class="fare">₱25.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-2" onclick="tricycleMap(2)">
+                          <p class="route">Holy Spirit to CSM</p>
+                          <p class="fare">₱30.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-3" onclick="tricycleMap(3)">
+                          <p class="route">Holy Spirit to Sports Complex</p>
+                          <p class="fare">₱40.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-4" onclick="tricycleMap(4)">
+                          <p class="route">One Big Wash to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
+                          <p class="fare">₱15.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-5" onclick="tricycleMap(5)">
+                          <p class="route">One Big Wash to CSM</p>
+                          <p class="fare">₱20.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-6" onclick="tricycleMap(6)">
+                          <p class="route">One Big Wash to Sports Complex</p>
+                          <p class="fare">₱20.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-7" onclick="tricycleMap(7)">
+                          <p class="route">Admin to CSM</p>
+                          <p class="fare">₱15.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-8" onclick="tricycleMap(8)">
+                          <p class="route">Admin to Sports Complex</p>
+                          <p class="fare">₱20.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-9" onclick="tricycleMap(9)">
+                          <p class="route">CSM to Sports Complex</p>
+                          <p class="fare">₱15.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-10" onclick="tricycleMap(10)">
+                          <p class="route">Bago Oshiro to ADMIN Building, EBL Dorm, Kalimudan, Library</p>
+                          <p class="fare">₱25.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-11" onclick="tricycleMap(11)">
+                          <p class="route">Bago Oshiro to CSM</p>
+                          <p class="fare">₱30.00</p>
+                      </div>
+                      <div class="fare-row" id="tricycle-12" onclick="tricycleMap(12)">
+                          <p class="route">Bago Oshiro to Sports Complex</p>
+                          <p class="fare">₱35.00</p>
+                      </div>
+                  </div>
+              </div>`;
+      }
+
+      // Add the visible class to trigger the fade-in effect
+      matrixDiv.classList.add("visible");
+  }, 500); // Match this timeout with the CSS transition duration
 }
 
 // Set default to Jeepney on page load
@@ -131,3 +141,4 @@ function tricycleMap(n) {
       break;
   }
 }
+
